828b041ed9304b2cb409fbec0df5a6263af416f0
Nikolai Stoilov <nikolai.stoilov@certivox.com>
2018-07-16 11:55:59 +0300

    1

--- components/chart.js
+++ components/chart.js
@@ -6,14 +6,16 @@ import store from '../mobx/Store';
 @observer
 export default class Chart extends Component {
   render() {
-    const fill = 'rgb(134, 65, 244)';
+    const fill = 'grey';
     const first = store.getTodayEvents();
     const data = [store.goal.times, first, 40, 30, 6];
+    const keys = ['1', '2', '3', '4'];
 
     return (
       <BarChart
         style={{ height: 200 }}
         data={data}
+        keys={keys}
         svg={{ fill }}
         contentInset={{ top: 30, bottom: 30 }}
       >
